185145451045902ca7cfc0330c13b6bf8bcfae01
omidreza-amrollahi <72734549+omidreza-amrollahi@users.noreply.github.com>
2022-05-24 23:45:11 +0200

    initial commit

---
+++ machine-learning-ex7-K-means Clustering and Principal Component Analysis/ex7/findClosestCentroids.m
@@ -0,0 +1,53 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+b=10^5;
+ab=0;
+m = size(X, 1);
+n = size(X, 2);
+c = zeros(n);
+
+for i=1:m
+    for j=1:K
+        c=0;
+        for L=1:n
+            c(L)=((X(i,L)-centroids(j,L))^2);
+        end
+            ab=sum(c);
+            if b>=ab
+                b=ab;
+                idx(i)=j;
+            end
+    end
+    ab=0;
+    b=10^5;
+end
+
+
+
+
+
+
+% =============================================================
+
+end
+
